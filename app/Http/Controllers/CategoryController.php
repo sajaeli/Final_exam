bb853038f5c2d89ffc9b8d7fe7a89af10f9b2803
sajaeli <81250937+sajaeli@users.noreply.github.com>
2021-06-23 14:43:07 +0300

    Create CategoryController.php

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categorys = Category::all();
+
+        return view('category.index', compact('categorys'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {;
+        Category::create([
+            'name' => $request->name,
+        ]);
+
+        return redirect()->route('category.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category,$id)
+    {
+        $category = category::find($id);
+        return view('category.edit', compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category,$id)
+    {
+        $category = category::find($id);
+        $category->update([
+            'name' => $request->name,
+        ]);
+
+        return redirect()->route('category.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category,$id)
+    {
+        $category = category::find($id);
+        $category->delete();
+        return redirect()->route('category.index');
+    }
+}
